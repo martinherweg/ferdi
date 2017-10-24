8b0147c8f777550e5855ad24d223001930a58da6
Martin Herweg <martin@martinherweg.de>
2017-10-25 00:37:49 +0200

    extended configuation file

--- src/.modlrrc.js
+++ src/.modlrrc.js
@@ -1,16 +1,55 @@
+const defaults = {
+  template: true,
+  css: true,
+  javascript: true,
+  vue: false,
+  fractal: false,
+};
+
 module.exports = {
-  defaults: {
-    html: true,
-    css: true,
-    javascript: true,
-    vue: false,
-    fractal: false,
+  files: {
+    template: {
+      name: '',
+      postfix: 'template',
+      extension: 'html',
+      description: 'Modlr should create a Template File',
+      default: defaults.template,
+    },
+    css: {
+      name: '',
+      postfix: 'style',
+      extension: 'scss',
+      description: 'Modlr should create Stylesheet File',
+      default: defaults.css,
+    },
+    javascript: {
+      name: '',
+      postfix: 'script',
+      extension: 'js',
+      description: 'Modlr should create JavaScript File',
+      default: defaults.javascript,
+    },
+    vue: {
+      name: '',
+      postfix: '',
+      extension: 'vue',
+      default: defaults.vue,
+    },
+    fractal: {
+      name: '',
+      postfix: 'config',
+      extension: 'js',
+      default: defaults.fractal,
+    },
   },
-  templateExtension: '.html',
-  scriptExtension: '.js',
-  cssExtension: '.css',
   paths: {
     templateBase: 'tmpl/',
-    modulePath: 'src/modules/',
+    modulePath: 'src/',
+    pathOptions: {
+      atomic: 'atomic/',
+      modules: 'modules/',
+    },
   },
 };
+
+exports.defaults = defaults;
